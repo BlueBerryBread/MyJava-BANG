eeea2df9d9c8d1e1a5164efc0b1b813bb1a07f5c
Cezar Craciunoiu <craciunoiu.cezar@yahoo.com>
2019-08-11 19:01:43 +0300

    Added basic server functionality - not tested

--- connection/Client.java
+++ connection/Client.java
@@ -1,1 +1,27 @@
-//TODO
+package connection;
+import java.net.*;
+import java.io.*;
+
+class Client {
+    private Socket clientSocket;
+    private DataOutputStream outToServer;
+
+    Client(String serverIP, Integer port) throws IOException, UnknownHostException {
+        clientSocket = new Socket(serverIP, port);
+        outToServer = new DataOutputStream(clientSocket.getOutputStream());
+    }
+
+    public void sendToServer(String sentence) throws IOException {
+        outToServer.writeBytes(sentence);
+    }
+
+    public String receiveFromServer() throws IOException {
+        BufferedReader inFromServer = new BufferedReader(
+                                      new InputStreamReader(clientSocket.getInputStream()));
+        return inFromServer.readLine();
+    }
+
+    public void closeConnection() throws IOException {
+        clientSocket.close();
+    }
+}
--- connection/Server.java
+++ connection/Server.java
@@ -1,1 +1,27 @@
-//TODO
+package connection;
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    private ServerSocket socket;
+    
+    public Server(Integer port) throws IOException {
+        socket = new ServerSocket(port);
+    }
+
+    // TODO game maybe needs multiplexing (turn-based -> not strictly necessary)
+    public void startServer() throws IOException {
+        while(true) {
+            Socket connectionSocket = socket.accept();
+            BufferedReader inFromClient =
+            new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            String sentence = inFromClient.readLine();
+            // TODO modify game state according to sentence
+            // TODO send new game state
+            // TODO if game is finished exit loop
+            
+            outToClient.writeBytes(sentence);
+        }
+    }
+}
